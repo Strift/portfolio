31857776d772ab3d02788fa1c8f06f9d4ae1aadd
Strift <lau.cazanove@gmail.com>
2019-05-03 20:28:41 +0200

    Add title

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -2,6 +2,9 @@ const containerPlugin = require('markdown-it-container')
 const figuresPlugin = require('markdown-it-implicit-figures')
 
 module.exports = {
+  title: 'Laurent Cazanove',
+  description: 'Freelance in esports and web development - Paris',
+  base: '/',
   markdown: {
     config: md => {
       md.use(containerPlugin, 'comment'),
--- content/articles/README.md
+++ content/articles/README.md
@@ -1,3 +1,5 @@
 ---
 layout: ArticleListLayout
 ---
+
+# Articles
