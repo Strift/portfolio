20b3d04a2a2ba7c008a8821deff3017497275574
Strift <lau.cazanove@gmail.com>
2019-06-26 10:16:07 +0200

    Add Google Analytics configuration

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -4,6 +4,7 @@ const figuresPlugin = require('markdown-it-implicit-figures')
 module.exports = {
   title: 'Laurent Cazanove',
   description: 'Freelance in esports and web development - Paris',
+  ga: 'UA-99085502-2',
   base: '/',
   markdown: {
     config: md => {
