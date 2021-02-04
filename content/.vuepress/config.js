ae710be64e015c1f9452b19852d13daf79b74aa9
Strift <lau.cazanove@gmail.com>
2021-02-04 12:49:49 +0100

    Add Twitter conversation tracking

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -4,8 +4,16 @@ const figuresPlugin = require('markdown-it-implicit-figures')
 module.exports = {
   title: 'Laurent Cazanove',
   description: 'Freelance in esports and web development - Paris',
-  ga: 'UA-99085502-2',
   base: '/',
+  head: [ ['script', {}, `
+    !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+    },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+    a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+    // Insert Twitter Pixel ID and Standard Event data below
+    twq('init','nzfbg');
+    twq('track','PageView');
+  `]],
+  ga: 'UA-99085502-2',
   markdown: {
     config: md => {
       md.use(containerPlugin, 'comment'),
