374c561f24b663276f5654952f625912b8b2d7f3
Strift <lau.cazanove@gmail.com>
2021-03-11 13:10:43 +0100

    Add Launching a recruitment platform for Classic guilds article

--- content/.vuepress/theme/layouts/ArticleListLayout.vue
+++ content/.vuepress/theme/layouts/ArticleListLayout.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container mx-auto mt-10 md:mt-16">
     <div class="flex flex-wrap">
-      <div v-for="article in articles" class="w-full md:w-1/2 lg:w-1/3 flex-none px-6 mb-10 md:mb-16">
+      <div v-for="article in articles" class="w-full md:w-1/2 lg:w-1/3 flex-none px-6 mb-10 md:mb-16" :key="article.path">
         <div class="card block shadow hover:shadow-lg ">
           <a v-if="article.external" class="text-black no-underline" :href="article.path" target="_blank">
             <ArticleCard
               :title="article.title"
               :thumbnail-url="article.thumbnail"
               :date="article.date"
-              lang="fr"
+              :lang="article.lang"
             />
           </a>
           <router-link v-else :to="article.path" class="text-black no-underline">
@@ -28,6 +28,7 @@
 <script>
 import moment from 'moment'
 import ogArticles from '../../ogamingArticles.json'
+import mediumArticles from '../../mediumArticles.json'
 
 import ArticleCard from '../components/ArticleCard'
 import SocialsFooter from '../components/SocialsFooter'
@@ -51,6 +52,17 @@ export default {
           return {
             path: a.url,
             thumbnail: `/images/articles/og-thumbnails/${a.image}`,
+            title: a.title,
+            date: a.date,
+            external: true,
+            lang: 'fr',
+            highlight: a.highlight
+          }
+        }))
+        .concat(mediumArticles.map(a => {
+          return {
+            path: a.url,
+            thumbnail: `/images/articles/medium-thumbnails/${a.image}`,
             title: a.title,
             date: a.date,
             external: true,
