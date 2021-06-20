b21feae126fc9bc73e8856e9f00ad3d978ae9d84
Strift <lau.cazanove@gmail.com>
2021-06-20 15:32:46 +0200

    Redesign Navbar

---
+++ components/NavbarLink.vue
@@ -0,0 +1,38 @@
+<template>
+  <NuxtLink
+    v-slot="{ href, navigate, isExactActive }"
+    :to="to"
+    :title="title"
+  >
+    <span
+      :class="{ 'text-sky-800': !isExactActive, 'text-gray-700': isExactActive }"
+      class="flex hover:text-sky-600 items-center space-x-1"
+    >
+      <ChevronDoubleRightIcon v-if="isExactActive" />
+      <a :href="href" @click="navigate">
+        <slot name="default" />
+      </a>
+    </span>
+  </NuxtLink>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+import ChevronDoubleRightIcon from '~/components/icons/outline/ChevronDoubleRightIcon.vue'
+
+export default defineComponent({
+  components: {
+    ChevronDoubleRightIcon
+  },
+  props: {
+    to: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
