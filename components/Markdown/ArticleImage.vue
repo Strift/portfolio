c5610901de51615479d238d8d74b429e63c313cd
Strift <lau.cazanove@gmail.com>
2024-01-08 22:28:21 +0400

    add 2023 article

---
+++ components/Markdown/ArticleImage.vue
@@ -0,0 +1,41 @@
+<template>
+  <figure>
+    <BaseImage
+      :src="src"
+      :alt="alt"
+      :title="title"
+      :width="`${width}`"
+      :height="`${height}`"
+      class="mx-auto"
+    />
+    <figcaption v-if="$slots.default">
+      <slot />
+    </figcaption>
+  </figure>
+</template>
+
+<script>
+import PropTypes from '@znck/prop-types'
+import BaseImage from '~/components/Base/Image.vue'
+
+export default {
+  components: {
+    BaseImage
+  },
+  props: {
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    caption: PropTypes.string,
+    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    height: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  },
+  mounted () {
+    console.log(this.height, this.width)
+  }
+}
+</script>
+
+<style>
+
+</style>
