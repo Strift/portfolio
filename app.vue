1caf350403d083595d481272f59848e439b52b39
Strift <lau.cazanove@gmail.com>
2024-07-05 15:56:49 +0200

    update app layout

--- app.vue
+++ app.vue
@@ -2,10 +2,15 @@
 	<div class="max-w-2xl mx-auto px-4 py-8">
 		<div class="mb-8">
 			<NuxtLink
+				class="flex space-x-4 items-center"
 				to="/"
-				class="text-2xl font-title font-extrabold"
 			>
-				Laurent Cazanove
+				<!-- <NuxtImg
+					src="/images/me.jpg"
+					alt="Laurent Cazanove"
+					class="h-10 w-auto rounded-full"
+				/> -->
+				<span class="text-2xl font-title font-extrabold">Laurent Cazanove</span>
 			</NuxtLink>
 		</div>
 		<NuxtPage />
