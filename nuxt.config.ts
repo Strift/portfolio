d86003952e937ae9067c13c1cd637b3f19f628f4
Strift <lau.cazanove@gmail.com>
2024-07-05 14:42:35 +0200

    add compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/content", "@nuxt/image"]
+  modules: ["@nuxt/content", "@nuxt/image"],
+  compatibilityDate: "2024-07-05"
 })
