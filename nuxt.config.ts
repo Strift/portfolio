494af67c4aa3a4675b0bbebceabde00f71fd81bf
Strift <lau.cazanove@gmail.com>
2024-07-05 15:10:56 +0200

    install eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxt/content", "@nuxt/image"],
-  compatibilityDate: "2024-07-05"
-})
+	devtools: { enabled: true },
+	modules: ['@nuxt/content', '@nuxt/image', '@nuxt/eslint'],
+	compatibilityDate: '2024-07-05',
+	eslint: {
+		config: {
+			stylistic: {
+				indent: 2,
+				semi: true,
+			},
+		},
+	},
+});
