cf04cb32dd688074a3533180eea77581c424b6c8
Strift <lau.cazanove@gmail.com>
2024-07-06 23:05:38 +0200

    fix postcard img size

--- components/blog/PostCard.vue
+++ components/blog/PostCard.vue
@@ -36,15 +36,14 @@ const getNuxtImgProps = (nav: ArticleNav) => {
     v-bind="getNuxtLinkProps(props.post)"
     class="group block sm:flex items-center"
   >
-    <div class="w-[256px] h-[136px] mb-3 sm:mb-0 flex-shrink-0">
-      <div class="relative w-full h-full overflow-hidden rounded-lg">
-        <BaseImage
-          v-bind="getNuxtImgProps(props.post)"
-          aspect-ratio="256/136"
-        />
-      </div>
+    <div class="w-full sm:w-[256px] mb-3 sm:mb-0 flex-shrink-0">
+      <BaseImage
+        v-bind="getNuxtImgProps(props.post)"
+        aspect-ratio="256/136"
+        class="rounded-lg"
+      />
     </div>
-    <div class="px-2 sm:px-8">
+    <div class="sm:px-8">
       <div class="text-lg font-bold mb-1 sm:mb-2 leading-tight text-color-emphasis">
         {{ props.post.title }}
       </div>
