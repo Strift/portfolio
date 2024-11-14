2483b2324a96e19d6401916c60761b0097483742
Strift <lau.cazanove@gmail.com>
2024-11-14 20:49:13 +0800

    update buttons style

--- components/blog/AdButton.vue
+++ components/blog/AdButton.vue
@@ -10,7 +10,7 @@ defineProps<{
   <NuxtLink
     :to="to"
     target="_blank"
-    class="inline-flex items-center justify-center w-full px-2 py-1 space-x-2 text-white rounded-full hover:bg-slate-700 bg-slate-500"
+    class="inline-flex items-center justify-center w-full px-2 py-2 space-x-2 rounded-md hover:bg-slate-700 border-slate-700 border text-slate-700 hover:text-white transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2"
   >
     <span>
       <slot name="default" />
