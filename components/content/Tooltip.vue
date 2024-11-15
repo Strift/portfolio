9c560b0ea3eb02310927425ec75267c317348ef9
Strift <lau.cazanove@gmail.com>
2024-11-15 20:18:53 +0800

    add tooltip component

---
+++ components/content/Tooltip.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { ICONS } from '~/constants'
+</script>
+
+<template>
+  <div class="flex items-center space-x-2 ">
+    <ContentSlot />
+    <VTooltip
+      :triggers="['hover', 'touch', 'click']"
+      class="inline-flex"
+    >
+      <Icon
+        :name="ICONS.TOOLTIP"
+        class="text-slate-500 h-5 w-5"
+      />
+      <template #popper>
+        <slot name="tooltip" />
+      </template>
+    </VTooltip>
+  </div>
+</template>
