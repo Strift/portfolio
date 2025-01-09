33ab336116535a4ca811a66e0fe384f7655617ee
Strift <lau.cazanove@gmail.com>
2025-01-09 22:40:24 +0800

    Update CTA on mobile

--- components/CTAButtons.vue
+++ components/CTAButtons.vue
@@ -8,13 +8,13 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="mt-6 flex flex-row gap-4 sm:gap-6 cta-buttons">
+  <div class="mt-6 flex flex-row flex-wrap gap-4 sm:gap-6 cta-buttons">
     <NuxtLink
       v-for="action in props.actions"
       :key="action.text"
       :href="action.href"
       target="_blank"
-      class="inline-flex items-center px-4 py-2 no-underline transition-colors duration-300 border rounded-md text-slate-700 border-slate-700 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-400 hover:text-white"
+      class="inline-flex items-center px-4 py-2 no-underline transition-colors duration-300 border rounded-md text-slate-700 border-slate-700 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-400 hover:text-white min-w-36 grow justify-center"
     >
       <Icon
         :name="ICONS[action.icon]"
--- components/blog/AboutMe.vue
+++ components/blog/AboutMe.vue
@@ -5,7 +5,7 @@ const { data: content } = await useAsyncData('home', () => queryContent<HomePage
 </script>
 
 <template>
-  <div
+  <section
     v-if="content"
     class="px-5 py-6 bg-slate-100 rounded-xl about-content"
   >
@@ -23,5 +23,5 @@ const { data: content } = await useAsyncData('home', () => queryContent<HomePage
       class="sm:hidden"
     />
     <CTAButtons :actions="content.actions" />
-  </div>
+  </section>
 </template>
