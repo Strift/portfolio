da4751fab178d52fe2df9e8d61edb1767255ffe5
Strift <lau.cazanove@gmail.com>
2025-06-10 20:33:07 +0800

    Add article navigation

--- pages/opensource-esports.vue
+++ pages/opensource-esports.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import type { MarkdownArticleNav } from '~/types'
+
 const { data: awesomeEsports } = await useAsyncData('awesome-esports', () =>
   queryContent('/awesome/awesome-esports').findOne(),
 )
@@ -11,6 +13,23 @@ function getGitHubOgImage(url: string) {
   const repo = match[2]
   return `https://opengraph.githubassets.com/1/${owner}/${repo}`
 }
+
+const POSTS_ATTRIBUTES = ['title', 'description', 'cover', 'coverAlt', '_path', 'date'] as (keyof MarkdownArticleNav)[]
+const MAX_POSTS = 3
+
+const { data: gamingPosts } = await useAsyncData(
+  `read-more-opensource-esports`,
+  () => queryContent<MarkdownArticleNav>('blog')
+    .only(POSTS_ATTRIBUTES)
+    .where({
+      tags: { $containsAny: ['esports', 'gaming'] },
+    })
+    .sort({
+      date: -1,
+    })
+    .limit(MAX_POSTS)
+    .find(),
+)
 </script>
 
 <template>
@@ -19,7 +38,7 @@ function getGitHubOgImage(url: string) {
       <h1 class="text-4xl font-bold mb-4 text-color-emphasis">
         Open-source Esports Repositories
       </h1>
-      <p class="max-w-2xl mx-auto text-color-muted">
+      <p class="max-w-2xl mx-auto text-color">
         A curated list of open-source esports-related repositories on GitHub.
       </p>
     </header>
@@ -69,5 +88,21 @@ function getGitHubOgImage(url: string) {
         >Awesome Esports</a> project.
       </div>
     </footer>
+    <h2 class="heading-2 mb-4">
+      ✍️ Related posts
+    </h2>
+    <div class="text-color mb-6">
+      Read my other articles.
+    </div>
+    <div class="overflow-x-auto pb-4 sm:pb-0 mb-4 sm:mb-6">
+      <div class="flex flex-row min-w-max sm:min-w-0 space-x-8 sm:block sm:space-x-0 sm:space-y-8">
+        <BlogPostCard
+          v-for="nav in gamingPosts"
+          :key="nav.title"
+          :post="nav"
+          class="w-[70vw] sm:max-w-full sm:w-full"
+        />
+      </div>
+    </div>
   </div>
 </template>
